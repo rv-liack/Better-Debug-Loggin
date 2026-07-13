26d7f5a30d2806ba2638d699d104a846d038eea0
Rafael <138715914+rv-liack@users.noreply.github.com>
2026-07-13 19:14:52 -0400

    Fix path format for logger registry autoload

--- addons/better-debug-logging/plugin.gd
+++ addons/better-debug-logging/plugin.gd
@@ -6,7 +6,7 @@ extends EditorPlugin
 const AUTOLOAD_NAME = "LoggerRegistry"
 
 func _enter_tree() -> void:
-	add_autoload_singleton(AUTOLOAD_NAME, "res://addons/better_debug_logging/logger_registry.gd")
+	add_autoload_singleton(AUTOLOAD_NAME, "res://addons/better-debug-logging/logger_registry.gd")
 
 func _exit_tree() -> void:
 	remove_autoload_singleton(AUTOLOAD_NAME)
